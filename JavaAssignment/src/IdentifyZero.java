ebdaf2552760796dda31d3fd0fa9d8fe75f8cac1
nikhil-rastogi-jft <128125335+nikhil-rastogi-jft@users.noreply.github.com>
2023-03-22 15:49:04 +0530

    Update IdentifyZero.java

--- JavaAssignment/src/IdentifyZero.java
+++ JavaAssignment/src/IdentifyZero.java
@@ -1,24 +1,20 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class IdentifyZero {
-    public static void main(String[] args) {
-        Random random=new Random();
-        int arr[]=new int[]{1,2,3,4,5,6,7,8,9,10};
-        int max=arr[0];
-        
-            for(int i=1;i<10;i++)
-            if(max<arr[i])
-                max=arr[i];
-        
-            Boolean []hash=new Boolean[max+1];
-        for(int i=0;i<arr.length;i++) hash[arr[i]]=true;
-        
+
+        public static void main(String[] args) {
+            Random random=new Random();
+            Scanner sc=new Scanner(System.in);
+            int n=10 ,arr[]=new int[n];
+            System.out.print(" enter the elements");
+            for(int i=0;i<n;i++)
+                arr[i]=sc.nextInt();
+            int bsum=0;
+            for(int i=0;i<arr.length;i++) bsum+=arr[i];
             arr[random.ints(1,10).findFirst().getAsInt()]=0;
-        
-            for(int i=0;i<arr.length;i++) hash[arr[i]]=false;
-        
-            for(int i=0;i<hash.length;i++)
-            if(hash[i])
-            System.out.println("missing number :"+i);
-    }
+            int asum=0;
+            for(int i=0;i<arr.length;i++) asum+=arr[i];
+            System.out.println("missing number is:"+(bsum-asum));
+        }
 }
