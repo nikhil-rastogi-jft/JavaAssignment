60f512e4603178ffd5bd77e42900c88a41bcaebf
nikhil-rastogi-jft <128125335+nikhil-rastogi-jft@users.noreply.github.com>
2023-03-22 13:26:15 +0530

    Update IdentifyZero.java

--- JavaAssignment/src/IdentifyZero.java
+++ JavaAssignment/src/IdentifyZero.java
@@ -1,15 +1,24 @@
 import java.util.Random;
 
 public class IdentifyZero {
-        public static Integer missing(int arr[]){
-            for(int i=0;i< arr.length;i++)
-                if(arr[i]==0) return i+1;
-            return null;
-        }
-        public static void main(String[] args) {
-            Random random=new Random();
-            int arr[]=new int[]{1,2,3,4,5,6,7,8,9,10};
+    public static void main(String[] args) {
+        Random random=new Random();
+        int arr[]=new int[]{1,2,3,4,5,6,7,8,9,10};
+        int max=arr[0];
+        
+            for(int i=1;i<10;i++)
+            if(max<arr[i])
+                max=arr[i];
+        
+            Boolean []hash=new Boolean[max+1];
+        for(int i=0;i<arr.length;i++) hash[arr[i]]=true;
+        
             arr[random.ints(1,10).findFirst().getAsInt()]=0;
-            System.out.println("missing number is:"+missing(arr));
-        }
+        
+            for(int i=0;i<arr.length;i++) hash[arr[i]]=false;
+        
+            for(int i=0;i<hash.length;i++)
+            if(hash[i])
+            System.out.println("missing number :"+i);
+    }
 }
